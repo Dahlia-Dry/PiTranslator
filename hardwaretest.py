80d278543e250fb7fa51a6ee7b7dfefcb2c7ad07
Dahlia Dry <dahliadry@MacBook-Pro.local>
2020-06-13 17:20:12 -0400

    added 2nd display

--- hardwaretest.py
+++ hardwaretest.py
@@ -4,24 +4,36 @@ import board
 from adafruit_epd.epd import Adafruit_EPD
 from PIL import Image, ImageDraw, ImageFont
 import RPi.GPIO as GPIO
-
-spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-ecs = digitalio.DigitalInOut(board.D5)
-dc = digitalio.DigitalInOut(board.D22)
-rst = digitalio.DigitalInOut(board.D27)
-busy = digitalio.DigitalInOut(board.D4)
-srcs = None
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(6, GPIO.OUT)
 from adafruit_epd.il0373 import Adafruit_IL0373
-display = Adafruit_IL0373(104,212, spi, cs_pin=ecs, dc_pin=dc, sramcs_pin=srcs,
-                          rst_pin=rst, busy_pin=busy)
-#for flexible display:
-display.set_black_buffer(1, False)
-display.set_color_buffer(1, False)
 
+#EINK 1---------------------------------------------------------------------
+spi1 = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+ecs1 = digitalio.DigitalInOut(board.D5)
+dc1 = digitalio.DigitalInOut(board.D22)
+rst1 = digitalio.DigitalInOut(board.D27)
+busy1 = digitalio.DigitalInOut(board.D4)
+srcs1 = None
+
+display1 = Adafruit_IL0373(104,212, spi1, cs_pin=ecs1, dc_pin=dc1, sramcs_pin=srcs1,
+                          rst_pin=rst1, busy_pin=busy1)
+#for flexible display:
+display1.set_black_buffer(1, False)
+display1.set_color_buffer(1, False)
+#---------------------------------------------------------------------------
+
+#EINK 2 --------------------------------------------------------------------
+spi2 = busio.SPI(board.SCK_1, MOSI=board.MOSI_1, MISO=board.MISO_1)
+ecs2 = digitalio.DigitalInOut(board.D26)
+dc2 = digitalio.DigitalInOut(board.D13)
+rst2 = digitalio.DigitalInOut(board.D23)
+busy2 = digitalio.DigitalInOut(board.D24)
+srcs2 = None
+display2 = Adafruit_IL0373(104,212, spi2, cs_pin=ecs2, dc_pin=dc2, sramcs_pin=srcs2,
+                          rst_pin=rst2, busy_pin=busy2)
+#for flexible display:
+display2.set_black_buffer(1, False)
+display2.set_color_buffer(1, False)
+#----------------------------------------------------------------------------
 # First define some color constants
 WHITE = (0xFF, 0xFF, 0xFF)
 BLACK = (0x00, 0x00, 0x00)
@@ -35,7 +47,7 @@ TEXT_COLOR = BLACK
 
 display.rotation = 1
 
-def pil_example():
+def pil_example(display):
 	image = Image.new("RGB", (display.width, display.height))
 
 	# Get drawing object to draw on image.
@@ -65,17 +77,16 @@ def pil_example():
 
 	# Display image.
 	print('displaying image:')
-	GPIO.output(6,GPIO.HIGH)
 	display.image(image)
 	display.display()
 
-def mono_test():
+def mono_test(display):
 	display.fill(Adafruit_EPD.WHITE)
 	display.fill_rect(0, 0, 50, 60, Adafruit_EPD.BLACK)
 	display.hline(80, 30, 60, Adafruit_EPD.BLACK)
 	display.vline(80, 30, 60, Adafruit_EPD.BLACK)
 	print('displaying image:')
-	GPIO.output(6,GPIO.HIGH)
 	display.display()
 
-pil_example()
+pil_example(display1)
+pil_example(display2)
